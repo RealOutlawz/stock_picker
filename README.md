a6fdbcc5324727baac0d2b47c5f035829e58b13b
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-17 00:07:15 -0500

    Updated README, refactored stock_picker method for clarity and improved variable naming

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stock_picker
+
+This is a Ruby program that implements as stock_picker function that takes a given array of stock prices and determines the best days to buy and sell for maximum profit. When you run the function it returns the those days as a pair in the format [a,b], the first value being the best day to buy and the second value being the best day to sell.
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,28 +1,22 @@
 def stock_picker(array)
   best_profit = 0
-  best_sell = 0
-  best_buy = 0
+  sell_date= 0
+  buy_date = 0
 
-  array.each_with_index do |val, idx|  # iterates over each element in the array
-    (idx+1...array.length).each do |temp_idx| # temp_idx is given the value of the outer arrays index plus 1 and the loop goes until the end of the array (array.length)
-      temp_val = array[temp_idx] # assigns temp_val to the value of the array at index temp_idx (outer index + 1)
-      profit = temp_val - val # profit is assinged to temp_val - val
+  array.each_with_index do |outer_value, outer_index| # iterates over each element in the array
+    (outer_index + 1...array.length).each do |inner_index| # the range of the loop is from outer index + 1 to the end of the array
+      inner_value = array[inner_index] 
+      profit = inner_value - outer_value 
       if profit > best_profit
-        best_buy = idx
-        best_sell = temp_idx
+        buy_date = outer_index
+        sell_date = inner_index
         best_profit = profit
       end
     end
   end
   
-  puts best_buy
-  puts best_sell
-  puts best_profit
+  result = [buy_date, sell_date]
+  print result
 end
 
-
-
 stock_picker([17,3,6,9,15,8,6,1,10])
-
-# Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day.
-# It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
